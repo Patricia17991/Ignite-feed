24840ce90e0732cfb5064ccdd5a644ab5a98deb3
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-06-16 16:47:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 //jsx = Javascript + XML (semelhante ao html)
 
+import Post from './Post'
+
 function App() {
   return (
+     <>
      <Post/>
+     <Post/>
+     <Post/>
+     <Post/>
+     </>
   )
 }
 
 export default App
+
+//O componente nada mais é que uma função que retorna html.
