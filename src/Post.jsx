91d5dcad3c96d1f7b1609a37117c6305a3feafa4
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-06-16 17:05:29 -0300

    Update Post.jsx

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,7 +1,13 @@
-
+//No react quando queremos exibir o valor de uma variável JS dentro de uma tag html, preciso usar {}
 export function Post(props) {
     console.log(props);
-    return <p>Post</p>
+    return (
+        <>
+           <strong>{props.author}</strong>
+           <p>{props.content}</p>
+         </>
+    )
+
 }
 
 
