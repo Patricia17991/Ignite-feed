db937cc1c5d9aa5adaaf06b0ac52c164ed987c5a
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-06-19 18:13:35 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css'
 
 export function Header() {
     return(
-         <strong className={styles.header}>Ignite Feed</strong>
+        <strong className={styles.header}>Ignite Feed</strong>
     );
 }
 
@@ -10,3 +10,4 @@ export function Header() {
 
 //no reactjs a gente não usa o class="" dentro dos elementos html. Porque como estamos dentro de um arquivo JS existe a className
 //quando usamos o css module preciso na importação dar um nome para os estilos.
+// o .header é o nom da classe que criei no arquivo de estilização.
