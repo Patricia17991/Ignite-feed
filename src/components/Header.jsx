cc7c5e41d55539166d61cf5c74e86512ef9275be
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-06-19 18:10:33 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import './Header.module.css'
+import styles from './Header.module.css'
 
 export function Header() {
     return(
@@ -9,3 +9,4 @@ export function Header() {
 /*No react não vamos importar o css no html, todas as importações vão vir dos arquivos JS.*/
 
 //no reactjs a gente não usa o class="" dentro dos elementos html. Porque como estamos dentro de um arquivo JS existe a className
+//quando usamos o css module preciso na importação dar um nome para os estilos.
