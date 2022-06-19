c8bfb22ece3155ce75498cecaec8068cb44e3f79
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-06-19 18:05:55 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-/*No react não vamos importar o css no html, todas as importações vão vir dos arquivos JS.*/
+import './Header.module.css'
 
 export function Header() {
     return(
-        <Header>Ignite Feed</Header>
+        <strong className="header">Ignite Feed</strong>
     );
 }
 
-//no reactjs a gente não usa o class="" dentro dos elementos html.
+/*No react não vamos importar o css no html, todas as importações vão vir dos arquivos JS.*/
+
+//no reactjs a gente não usa o class="" dentro dos elementos html. Porque como estamos dentro de um arquivo JS existe a className
