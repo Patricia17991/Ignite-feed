a0882fb07090a27e0cba0e045617921aa3281226
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-06-19 18:07:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 //jsx = Javascript + XML (semelhante ao html)
 import {Post} from './Post'
 import './styles.css'
-
+import {Header} from './components/Header'
 //Quando estilizamos um componente reactjs precisamos prestar bastante atenção ao escopo.
 // Vamos usar o css module, o vite trás suporte para ele.
-
 
 function App() {
   return (
     <>
+     <Header/>
+
      <Post author="Patricia" 
      content="O componente nada mais é que uma 
      função que retorna html."
