35a0caa78727c05fe453e8a8e5c765d42bd45716
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-07-11 19:03:10 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,7 @@ export function Header() {
         <header className={styles.header}>
           <strong>Ignite Feed</strong>
         </header>
-        
     );
 }
 
-/*No react não vamos importar o css no html, todas as importações vão vir dos arquivos JS.*/
-
-//no reactjs a gente não usa o class="" dentro dos elementos html. Porque como estamos dentro de um arquivo JS existe a className
-//quando usamos o css module preciso na importação dar um nome para os estilos.
-// o .header é o nom da classe que criei no arquivo de estilização.
 // o css module gera um  rash para cada classe criada para que cada uma seja única e não interfira na estilização dos outros componentes.
