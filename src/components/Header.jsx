edb4a6ec972a147656a30ea3960723c0bdcaee7d
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-07-15 18:03:25 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import styles from './Header.module.css'
 
 export function Header() {
     return(
-       <header>
-        <strong className={styles.header}>Ignite Feed</strong>
+       <header className={styles.header}>
+          <strong>Ignite Feed</strong>
        </header>
     );
 }
