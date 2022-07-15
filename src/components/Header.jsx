9c4700c8cee7ccf4fe3f3b1542f6a82674d490fa
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-07-15 18:01:29 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,9 @@ import styles from './Header.module.css'
 
 export function Header() {
     return(
-        <header className={styles.header}>
-          <strong>Ignite Feed</strong>
-        </header>
+       <strong className={styles.header}>
+        Ignite Feed
+       </strong>
     );
 }
-
 // o css module gera um  rash para cada classe criada para que cada uma seja única e não interfira na estilização dos outros componentes.
